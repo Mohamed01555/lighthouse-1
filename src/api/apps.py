641ba1b6cb7a71b944e1fb1f2c7f96bdbef9ed50
assem <assemmorsy@outlook.com>
2021-04-19 13:30:05 +0200

    app models

--- src/api/apps.py
+++ src/api/apps.py
@@ -1,5 +1,7 @@
+from typing import DefaultDict
 from django.apps import AppConfig
 
 
 class MainConfig(AppConfig):
+    DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
     name = 'api'
--- src/api/models.py
+++ src/api/models.py
@@ -1,7 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class MissingPerson(models.Model):
-    name = models.CharField(max_length=20)
 
-class ContactPerson(models.Model):
+class KnownMissingPerson(models.Model):
+    name = models.CharField(max_length=50)
+    contactPerson = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="missingPersons"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class KnownMissingPersonImages(models.Model):
+    missingPerson = models.ForeignKey(
+        KnownMissingPerson, on_delete=models.CASCADE, related_name="photos"
+    )
+    imgPath = models.ImageField(upload_to="knownMissingPersonsImages")
+    imgEmbedding = models.JSONField()
+
+    def __str__(self):
+        return f"image for {self.missingPerson} taken by {self.takenBy}"
+
+
+class UserSeeknownMissingPerson(models.Model):
+    missingPerson = models.ForeignKey(
+        KnownMissingPerson, on_delete=models.CASCADE, related_name="photos"
+    )
+    imgPath = models.ImageField(upload_to="unknownMissingPersonsImages")
+    imgEmbedding = models.JSONField()
+    takenBy = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="takenPhotos", default=0
+    )
+
+    def __str__(self):
+        return f"image for {self.missingPerson} taken by {self.takenBy}"
+
+
+class UnKnownMissingPerson(models.Model):
     pass
+
+
+class UserSeeUnknownMissingPerson(models.Model):
+    missingPerson = models.ForeignKey(
+        UnKnownMissingPerson, on_delete=models.CASCADE, related_name="photos"
+    )
+    imgPath = models.ImageField(upload_to="unknownMissingPersonsImages")
+    imgEmbedding = models.JSONField()
+    takenBy = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="takenPhotos", default=0
+    )
+
+    def __str__(self):
+        return f"image for Unknown Missing person taken by {self.takenBy}"
